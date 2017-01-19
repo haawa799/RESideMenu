521588087c6c35ad45f1294a55f26b5d4e7cc14b
Andrew Kharchyshyn <haawaplus@gmail.com>
2017-01-19 12:14:49 +0200

    Seperate framework for Carthage integration

--- Examples/Simple/RESideMenuExample/DEMOAppDelegate.h
+++ Examples/Simple/RESideMenuExample/DEMOAppDelegate.h
@@ -7,7 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "RESideMenu.h"
+@import RESideMenuKit;
 
 @interface DEMOAppDelegate : UIResponder <UIApplicationDelegate, RESideMenuDelegate>
 
---
+++ Examples/Simple/RESideMenuKit/RESideMenuKit.h
@@ -0,0 +1,22 @@
+//
+//  RESideMenuKit.h
+//  RESideMenuKit
+//
+//  Created by Andriy K. on 1/19/17.
+//  Copyright © 2017 Roman Efimov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "RECommonFunctions.h"
+#import "RESideMenu.h"
+#import "UIViewController+RESideMenu.h"
+
+//! Project version number for RESideMenuKit.
+FOUNDATION_EXPORT double RESideMenuKitVersionNumber;
+
+//! Project version string for RESideMenuKit.
+FOUNDATION_EXPORT const unsigned char RESideMenuKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RESideMenuKit/PublicHeader.h>
+
+
